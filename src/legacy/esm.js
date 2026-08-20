ae537c6dd117acce8a711b95482e19dd887aa400
Nick Freear <nfreear@yahoo.co.uk>
2026-08-20 01:22:49 +0100

    Re-add legacy ESM code from `0.8.0-esm` tag (#6)

---
+++ src/legacy/esm.js
@@ -0,0 +1,10 @@
+/**
+ * Legacy ESM code.
+ */
+import SuperGif from './esm/SuperGif.esm.js';
+import lzwDecode from './esm/lzwDecode.js';
+import parseGIF from './esm/parseGIF.js';
+import Stream from './esm/Stream.js';
+import { bitsToNum, byteToBitArr } from '../util.js';
+
+export { SuperGif, lzwDecode, parseGIF, Stream, bitsToNum, byteToBitArr };
